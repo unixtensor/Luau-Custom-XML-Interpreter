95b37192226a0346e95108d8c0bf5d80958cdf77
interpreterK <rhpidfyre@tuta.io>
2023-02-24 23:47:55 -0500

    Comments, Type->Tag to respect XML naming convention(s)

--- src/XML.lua
+++ src/XML.lua
@@ -1,27 +1,27 @@
 --[[
 	The XML scripting structure:
 	"
-		<?xml version="1" author="author name" description="UI purposes"?> <!--This is optional-->	
-		<xml>
-			<!--With var just like in HTML, data-anyname will be a named variable that can be used-->
-			<var data-FrameColor=Color3.new(1,0,1)>
-			<var data-AnchorVector=Vector2.new(.5,.5)>
+<?xml version="1" author="author name" description="UI purposes"?> <!--This is optional-->	
+<xml>
+	<!--With var just like in HTML, data-anyname will be a named variable that can be used-->
+	<var data-FrameColor=Color3.new(1,0,1)>
+	<var data-AnchorVector=Vector2.new(.5,.5)>
 
-			<ScreenGui Parent=game:GetService("StarterGui") ResetOnSpawn=false>
-				<Frame BackgroundColor3=data-FrameColor AnchorPoint=data-AnchorVector>
-					<!--A comment about anything-->
-					<TextLabel>This Is Funny And Gonna Be Real</TextLabel>
-					<TextButton>
-						<!--Maybe some custom's too? mine as well get creative with our custom XML lang-->
-						<b>Im Bold and bright</b>
-					</TextButton>
-					<!--Pushing Limits already?-->
-					<script type="server/local/module">
-						print("Hello World From XML Lua.")
-					</script>
-				</Frame>
-			</ScreenGui>
-		</xml>
+	<ScreenGui Parent=game:GetService("StarterGui") ResetOnSpawn=false>
+		<Frame BackgroundColor3=data-FrameColor AnchorPoint=data-AnchorVector>
+			<!--A comment about anything-->
+			<TextLabel>This Is Funny And Gonna Be Real</TextLabel>
+			<TextButton>
+				<!--Maybe some custom's too? mine as well get creative with our custom XML lang-->
+				<b>Im Bold and bright</b>
+			</TextButton>
+			<!--Pushing Limits already?-->
+			<script type="server/local/module">
+				print("Hello World From XML Lua.")
+			</script>
+		</Frame>
+	</ScreenGui>
+</xml>
 	"
 	
 	The XML data structure:
@@ -69,10 +69,10 @@ local function IsAServicePath(potential_S)
 end
 
 local function Wrap_data_props(XML_props)
-	--Convert strings to services
 	local Wrap = {}
 	for prop, val in XML_props do
 		if prop == "Parent" then
+			--Convert strings to service
 			if val == "workspace" then
 				val = "Workspace" 
 			end
@@ -81,9 +81,11 @@ local function Wrap_data_props(XML_props)
 				Wrap[prop] = Service
 			end
 		elseif prop == "true" or prop == "false" then
+			--Convert strings to bool
 			--Idk.. this is pure prediction that this will be an actual bool and not a string as a bool
 			Wrap[prop] = val == "true" and true
 		elseif tonumber(val) then
+			--Convert strings to number(s)
 			Wrap[prop] = tonumber(val)
 		else
 			Wrap[prop] = val
@@ -101,22 +103,22 @@ XML.New = XML.new
 function XML:Interpreter()
 	local S = self.XML_Source
 	local Xpat = "<([%s%S]+)(.-)>(.-)</(%1)>"
-	for Type,RawAT,Value,_ in S:gmatch(Xpat) do
+	for Tag,RawAT,Value,_ in S:gmatch(Xpat) do
 		local raw_data = {
-			Type = Type, 
+			Tag = Tag, 
 			Value = Value,
 			Attr = RawAT:split(' ')
 		}
-		if raw_data.Attr[1] == "" then
+		if raw_data.Attr[1] == "" then --Clear a possible empty block
 			table.remove(raw_data.Attr, 1)
 		end
 		table.insert(self.__cache, raw_data)
 	end
 	for i = 1, #self.__cache do
 		local v = self.__cache[i]
-		if Valid_Insts[v.Type] then
+		if Valid_Insts[v.Tag] then
 			local Converted_data = Wrap_data_props(Get_XML_props(v.Attr))
-			RBLXCreate(v.Type, Converted_data)
+			RBLXCreate(v.Tag, Converted_data)
 		else
 			--Handle specials
 		end
