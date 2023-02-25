eeb987c51f6db0d4c6a39f8a4787e7b253ca1f12
interpreterK <rhpidfyre@tuta.io>
2023-02-25 15:06:45 -0500

    Structure and examples

---
+++ src/examples/example-1.client.lua
@@ -0,0 +1,9 @@
+local XML = [[]]
+
+local XML_Module = require(path.XML)
+
+local XML_Wrapper = XML_Module.New(XML)
+local XML_metadata = XML_Wrapper:Make()
+
+--Optionally log the metadata for debug purposes
+--print(XML_metadata)
